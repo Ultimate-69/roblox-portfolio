c473f73b1fbbe46d27a6765e99d6530adcbe7fca
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-12-25 19:08:44 +0200

    added new review

--- data/reviews-data.js
+++ data/reviews-data.js
@@ -1,4 +1,5 @@
 const reviews = 
 [
     'https://trello.com/1/cards/66253886b8377b34ef524c39/attachments/6625388b7777adaf9d57fe63/download/image.png',
+    'https://trello.com/1/cards/676c3bae0d7d025f8d53fc1e/attachments/676c3bb0696ac7bf573b6931/download/image.png'
 ]
