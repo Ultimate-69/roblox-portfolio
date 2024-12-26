0b647abed519eadbf3ce19ae516b106758f34bc3
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-12-26 21:27:12 +0200

    added more stuff

--- data/reviews-data.js
+++ data/reviews-data.js
@@ -2,5 +2,6 @@ const reviews =
 [
     'https://trello.com/1/cards/66253886b8377b34ef524c39/attachments/6625388b7777adaf9d57fe63/download/image.png',
     'https://trello.com/1/cards/676c3bae0d7d025f8d53fc1e/attachments/676c3bb0696ac7bf573b6931/download/image.png',
-    'https://trello.com/1/cards/676d70759aa638bd91a09316/attachments/676d70774dc7e0f57463b399/download/image.png'
+    'https://trello.com/1/cards/676d70759aa638bd91a09316/attachments/676d70774dc7e0f57463b399/download/image.png',
+    'https://trello.com/1/cards/676dade18b69659ff57daa92/attachments/676dade4a493f3e28e0bdc35/download/image.png',
 ]
