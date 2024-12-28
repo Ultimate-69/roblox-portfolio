79c1e320ca48c05cc0800d38970de162ca86f1e2
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-12-29 00:49:33 +0200

    added new review

--- data/reviews-data.js
+++ data/reviews-data.js
@@ -4,4 +4,5 @@ const reviews =
     'https://trello.com/1/cards/676c3bae0d7d025f8d53fc1e/attachments/676c3bb0696ac7bf573b6931/download/image.png',
     'https://trello.com/1/cards/676d70759aa638bd91a09316/attachments/676d70774dc7e0f57463b399/download/image.png',
     'https://trello.com/1/cards/676dade18b69659ff57daa92/attachments/676dade4a493f3e28e0bdc35/download/image.png',
+    'https://trello.com/1/cards/6770803fad0b9d7cdf73d2bf/attachments/67708041e3bd8b2b136c2214/download/image.png',
 ]
