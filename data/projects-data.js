8970b02f4564b3e92c8ebbc1b4c21cda2a18b5ca
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-01-06 16:46:10 +0200

    updated order of previous work

--- data/projects-data.js
+++ data/projects-data.js
@@ -1,24 +1,6 @@
 const projects = 
 [
     {
-        img: 'https://trello.com/1/cards/66241072e07dddf2fc62e026/attachments/662410c2cd02e8e633d0e8ed/download/image.png',
-        name: 'JJK Game [OLD]',
-        description: 'A Jujutsu Kaisen game that I made a while ago. Features a Main Menu, Combat, Dashes, Abilities, Etc.',
-        link: 'https://www.youtube.com/watch?v=_oEINKHji5s',
-    },
-    {
-        img: 'https://trello.com/1/cards/6629541ef1d2915e6dc4e7eb/attachments/67682e9d8a46e7293a0e284f/download/Screenshot_2024-12-22_171617.png',
-        name: 'Blox Fruits Ability System [OLD]',
-        description: 'I remade the Blox Fruits ability system and even optimized it for mobile but in a JJK setting.',
-        link: 'https://www.youtube.com/watch?v=ogXUUG0PYR4&t=7s',
-    },
-    {
-        img: 'https://trello.com/1/cards/66cb0eaa810379b64b11f1c2/attachments/66cb0ee797b5287df5203f40/download/image.png',
-        name: 'Blox Fruits Ability System [OLD-ISH]',
-        description: 'A very close copy of how blox fruits works, with working quests, a level up system, a working stat system, and even skills like the fruits. Mobile compatible too.',
-        link: 'https://youtu.be/PnJ2Yu1htOA'
-    },
-    {
         img: 'https://trello.com/1/cards/676affd9d70438276963c6b6/attachments/676affdc7c8416620493f276/download/image.png',
         name: 'Basic Combat System',
         description: 'A very basic combat system that supports melee, grabs, and weapons. It is made basic on purpose.',
@@ -29,5 +11,23 @@ const projects =
         name: 'Crouching System [COM]',
         description: 'A basic crouching system for a comission.',
         link: 'https://vimeo.com/1042154938',
+    },
+    {
+        img: 'https://trello.com/1/cards/66cb0eaa810379b64b11f1c2/attachments/66cb0ee797b5287df5203f40/download/image.png',
+        name: 'Blox Fruits Ability System [OLD-ISH]',
+        description: 'A very close copy of how blox fruits works, with working quests, a level up system, a working stat system, and even skills like the fruits. Mobile compatible too.',
+        link: 'https://youtu.be/PnJ2Yu1htOA'
+    },
+    {
+        img: 'https://trello.com/1/cards/6629541ef1d2915e6dc4e7eb/attachments/67682e9d8a46e7293a0e284f/download/Screenshot_2024-12-22_171617.png',
+        name: 'Blox Fruits Ability System [OLD]',
+        description: 'I remade the Blox Fruits ability system and even optimized it for mobile but in a JJK setting.',
+        link: 'https://www.youtube.com/watch?v=ogXUUG0PYR4&t=7s',
+    },
+    {
+        img: 'https://trello.com/1/cards/66241072e07dddf2fc62e026/attachments/662410c2cd02e8e633d0e8ed/download/image.png',
+        name: 'JJK Game [OLD]',
+        description: 'A Jujutsu Kaisen game that I made a while ago. Features a Main Menu, Combat, Dashes, Abilities, Etc.',
+        link: 'https://www.youtube.com/watch?v=_oEINKHji5s',
     },
 ];
